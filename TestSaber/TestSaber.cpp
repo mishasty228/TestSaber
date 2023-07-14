5ddfd2ffdef4e2d32eab88d94454c12bf459c674
mishasty228 <mishasty2001@gmail.com>
2023-07-14 17:52:26 +0300

    Final Serialize

--- TestSaber/TestSaber.cpp
+++ TestSaber/TestSaber.cpp
@@ -50,11 +50,29 @@ public:
         for (int i = 0; i < count; i++)
         {
             bool Found = Target.data.c_str() == Search.c_str();
-            //std::cout << "Comparing " << Target.data.c_str() << " with " << Search.c_str() << " returns " << Found << std::endl;
             if (Found) return &Target;
             Target = *Target.next;
         }
         return NULL;
+    }
+    int GetIndexAtNode(ListNode* Node)
+    {
+        ListNode* Target = head;
+        for (int i = 0; i < count; i++)
+        {
+            if (Node == Target) return i;
+            Target = Target->next;
+        }
+        return 0;
+    }
+    ListNode* GetNodeAtIndex(int Index)
+    {
+        ListNode* Target = head;
+        for (int i = 0; i < Index; i++)
+        {
+            Target = Target->next;
+        }
+        return Target;
     }
     ListNode* AddNode(std::string Data)
     {
@@ -97,32 +115,31 @@ void InitializeNodes(List* NodeList)
 
 void List::Serialize(FILE* file)
 {
-    ListNode CurrentNode = *head;
+    ListNode* CurrentNode = head;
 
     fwrite(&count, sizeof(count), 1, file);
     for (int i = 0; i < count; i++)
     {
-        unsigned int DataLen(CurrentNode.data.size());
+        unsigned int DataLen(CurrentNode->data.size());
         fwrite(&DataLen, sizeof(DataLen), 1, file);
-        fwrite(CurrentNode.data.c_str(), CurrentNode.data.length(), 1, file);
+        fwrite(CurrentNode->data.c_str(), CurrentNode->data.length(), 1, file);
 
-        CurrentNode = *CurrentNode.next;
+        CurrentNode = CurrentNode->next;
     }
-    CurrentNode = *head;
+    CurrentNode = head;
     for (int i = 0; i < count; i++)
     {
-        unsigned int RandDataLen(CurrentNode.rand->data.size());
-        fwrite(&RandDataLen, sizeof(RandDataLen), 1, file);
-        fwrite(CurrentNode.rand->data.c_str(), CurrentNode.rand->data.length(), 1, file);
+        int RandDataInd = GetIndexAtNode(CurrentNode->rand);
 
-        CurrentNode = *CurrentNode.next;
+        fwrite(&RandDataInd, sizeof(RandDataInd), 1, file);
+
+        CurrentNode = CurrentNode->next;
     }
 }
 
 void List::Deserialize(FILE* file)
 {
     ListNode* CurrentNode;
-    //head = &CurrentNode;
     unsigned int ListLen(0);
     fread(&ListLen, sizeof(ListLen), 1, file);
     count = ListLen;
@@ -144,59 +161,17 @@ void List::Deserialize(FILE* file)
     CurrentNode = head;
     for (int i = 0; i < count; i++)
     {
-        unsigned int RandDataLen(0);
+        int RandDataInd;
         std::string RandString = "";
-        fread(&RandDataLen, sizeof(RandDataLen), 1, file);
-        fread(&RandString[0], 1, RandDataLen, file);
-        CurrentNode->rand = FindElem(RandString);
+        fread(&RandDataInd, sizeof(RandDataInd), 1, file);
+        std::cout << "Random index: " << RandDataInd << std::endl;
+        CurrentNode->rand = GetNodeAtIndex(RandDataInd);
         
-        std::cout << CurrentNode->data.c_str() << "-------" << RandString.c_str() << std::endl;
+        std::cout << CurrentNode->data.c_str() << "-------" 
+            << CurrentNode->rand->data.c_str() << std::endl;
 
         CurrentNode = CurrentNode->next;
     }
-}
-
-void SerializeNode(ListNode Node)
-{
-    std::ofstream fout("Node.txt");
-    fout << Node.data << '!' << &Node.prev << '!' << &Node.next << '!' << &Node.rand;
-    fout.close();
-
-    std::ofstream fbout("Node.bin", std::ios_base::binary);
-    fbout.write((char*)&Node, sizeof(Node));
-    fbout.close();
-
-    FILE* stream;
-    errno_t err;
-    err = fopen_s(&stream, "NewNode.bin", "wb");
-    if (err == 0)
-    {
-        unsigned int DataLen(Node.data.size());
-        fwrite(&DataLen, sizeof(DataLen), 1, stream);
-        fwrite(Node.data.c_str(), Node.data.length(), 1, stream);
-        fwrite(&Node.prev, 1, sizeof(ListNode), stream);
-        fwrite(&Node.next, sizeof(ListNode), 1, stream);
-        fwrite(&Node.rand, sizeof(ListNode), 1, stream);
-    }
-    err = fclose(stream);
-}
-
-void DeserializeNode(ListNode Node)
-{
-    ListNode NewNode = ListNode();
-
-    FILE* stream;
-    errno_t err;
-    err = fopen_s(&stream, "NewNode.bin", "rb");
-    if (err == 0)
-    {
-        unsigned int DataLen(0);
-        fread(&DataLen, sizeof(DataLen), 1, stream);
-        fread(&Node.data[0], 1, DataLen, stream);
-    }
-    std::cout << Node.data << std::endl;
-
-    err = fclose(stream);
 }
 
 void ToBit(int n)
@@ -242,23 +217,12 @@ void RemoveDups(char* str)
 
 int main()
 {
-    /*ToBit(237);
-    ToBit(-163);*/
+    ToBit(237);
+    ToBit(-163);
 
-    /*char data[] = "PPPRRROO    GaaaaMMMMMerr";
+    char data[] = "PPPRRROO    GaaaaMMMMMerr";
     RemoveDups(data);
-    printf("%s\n", data);*/
-
-    /*ListNode Node1 = ListNode();    
-    ListNode Node2 = ListNode();
-    ListNode Node3 = ListNode();
-    ListNode Node4 = ListNode("Fourth");
-    Node4.prev = &Node3;
-    Node4.next = &Node4;
-    Node4.rand = &Node1;
-
-    SerializeNode(Node4);
-    DeserializeNode(Node4);*/
+    printf("%s\n", data);
 
     srand(time(NULL));
 
@@ -266,13 +230,28 @@ int main()
     FILE* stream;
     errno_t err;
 
-    /*InitializeNodes(&NodeList);
+    int Select = 0;
 
-    err = fopen_s(&stream, "NodeList.bin", "wb");
-    if (err == 0) NodeList.Serialize(stream);*/
+    std::cout << "Choose action (1 - Initialize and serialize, 2 - deserialize): ";
+    std::cin >> Select;
+    switch (Select)
+    {
+    case 1:
+        InitializeNodes(&NodeList);
 
-    err = fopen_s(&stream, "NodeList.bin", "rb");
-    if (err == 0) NodeList.Deserialize(stream);
+        err = fopen_s(&stream, "NodeList.bin", "wb");
+        if (err == 0) NodeList.Serialize(stream);
+        err = fclose(stream);
+        break;
+    case 2:
 
-    err = fclose(stream);
+        err = fopen_s(&stream, "NodeList.bin", "rb");
+        if (err == 0) NodeList.Deserialize(stream);
+        err = fclose(stream);
+        break;
+    default: 
+        break;
+    }
+    
+    return 0;
 }
